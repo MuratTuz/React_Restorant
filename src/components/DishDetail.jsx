ac994aca9b119834c0a34e319d873df95eadd2c6
MuratTuz <murattuzlali@gmail.com>
2020-06-26 14:58:08 +0200

    Container component is added

--- src/components/DishDetail.jsx
+++ src/components/DishDetail.jsx
@@ -9,6 +9,39 @@ import { Media } from 'reactstrap';
 
 class DishDetail extends React.Component {
 
+    renderDish(dish) {
+
+        return (
+            <Card key={"card" + dish.id}>
+                <CardImg top src={dish.image} alt={dish.name} />
+                <CardBody>
+                    <CardTitle>{dish.name}</CardTitle>
+                    <CardText>{dish.description}</CardText>
+                </CardBody>
+            </Card>
+        );
+    }
+    renderComments(dish) {
+        const dishComment = dish.comments.map((comment) => {
+            return (
+                <li key={"comment" + comment.id}>
+                    <p>{comment.comment}</p>
+                    <p>-- {comment.author},
+            {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'long', day: '2-digit' }).format(new Date(comment.date))}
+                    </p>
+                </li>
+            );
+        });
+
+        return (
+            <div className="col-12 col-md-5 m-1">
+                <h3>Comments</h3>
+                <ul className="list-unstyled">
+                    {dishComment}
+                </ul>
+            </div>
+        );
+    }
 
     render() {
         const dish = this.props.dish;
@@ -17,29 +50,11 @@ class DishDetail extends React.Component {
                 <div className="container" style={{ textAlign: 'left' }}>
                     <div className="row">
                         <div className="col-12 col-md-5 m-1">
-                            <Card key={"card" + dish.id}>
-                                <CardImg top src={dish.image} alt={dish.name} />
-                                <CardBody>
-                                    <CardTitle>{dish.name}</CardTitle>
-                                    <CardText>{dish.description}</CardText>
-                                </CardBody>
-                            </Card>
+                            {this.renderDish(dish)}
                         </div>
-                        <div className="col-12 col-md-5 m-1">
-                            <Media key={"key" + dish.id}>
-                                <Media body>
-                                    <Media heading>
-                                        Comments
-                                    </Media>
-                                    {dish.comments.map((comment) =>
-                                        <div key={"comment" + comment.id}>
-                                            <p>{comment.comment}</p>
-                                            <p>-- {comment.author}  {comment.date}</p>
-                                        </div>
-                                    )}
-                                </Media>
-                            </Media>
-                        </div>
+
+                        {this.renderComments(dish)}
+
                     </div>
                 </div>
             );
